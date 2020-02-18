2e041dc52249a99fcc690dc4e27100de2a54101f
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-18 15:11:56 -0800

    Fix redirection to make this component a class.

--- src/CreatePost.jsx
+++ src/CreatePost.jsx
@@ -1,49 +1,64 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { v4 } from 'uuid'
 import PropTypes from 'prop-types'
 import { Redirect } from 'react-router-dom'
 
-const CreatePost = props => {
-    let _title = null
-    let _body = null
-    
-    const handleNewForm = event => {
+class CreatePost extends Component {
+    state = {
+        redirect: false,
+        _title: null,
+        _body: null
+
+    }
+
+    handleNewForm = event => {
         event.preventDefault()
-        props.onCreateNewPost({
-            title: _title.value,
-            body: _body.value,
+        this.props.onCreateNewPost({
+            title: this._title.value,
+            body: this._body.value,
             id: v4(),
             upvote: 0,
             downvote: 0
         })
     }
 
-    const redirectHome = () => {
-        return <Redirect to='/' />
+    setRedirect = () => {
+        this.setState({
+            redirect: true
+        })
     }
 
-    return(
-        <main>
-            <h1>Create New Post</h1>
-            <form onSubmit={handleNewForm} className='form-container'>
-                <input
-                    type='text'
-                    placeholder='Post title'
-                    ref={input => {_title = input}}    
-                />
-                <textarea
-                    type='text'
-                    placeholder='Post content'
-                    ref={textarea => {_body = textarea}}
-                ></textarea>
-                <button type='submit' onClick={redirectHome}>Post</button>
-            </form>
-        </main>
-    )
+    renderRedirect = () => {
+        if(this.state.redirect) {
+            return <Redirect to='/' />
+        }
+    }
+
+    render() {
+        return(
+            <main>
+                <h1>Create New Post</h1>
+                <form onSubmit={this.handleNewForm} className='form-container'>
+                    <input
+                        type='text'
+                        placeholder='Post title'
+                        ref={input => {this._title = input}}    
+                    />
+                    <textarea
+                        type='text'
+                        placeholder='Post content'
+                        ref={textarea => {this._body = textarea}}
+                    ></textarea>
+                    {this.renderRedirect()}
+                    <button type='submit' onClick={this.setRedirect}>Post</button>
+                </form>
+            </main>
+        )
+    }
 }
 
-CreatePost.propTypes = {
-    onCreateNewPost: PropTypes.func   
-}
+// CreatePost.propTypes = {
+//     onCreateNewPost: PropTypes.func   
+// }
 
 export default CreatePost
