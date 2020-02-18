c74ff2ff17b217c7396db8be770d56382fafa511
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-18 13:32:40 -0800

    Add state and handle upvote and downvote method to App component and pass it down to Forum.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
+import { v4 } from 'uuid'
 import './App.css';
 import Forum from './Forum'
+import NotFound from './NotFound'
+import CreatePost from './CreatePost'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-      <Switch>
-        <Route exact path='/' component={Forum} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      body: '',
+      postId: v4(),
+      upvote: 0,
+      downvote: 0
+    }
+  }
+
+  handleUpVote = () => {
+    this.setState({
+      upvote: this.state.upvote + 1,
+      ...this.state.stateObj,
+    })
+  }
+
+  handleDownVote = () => {
+    this.setState({
+      downvote: this.state.downvote + 1,
+      ...this.state.stateObj,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <button onClick={this.handleUpVote}>Like</button>
+      <button onClick={this.handleDownVote}>Meh</button>
+        <p>{this.state.title}</p>
+        <p>{this.state.body}</p>
+        <p>Upvote{this.state.upvote}</p>
+        <p>DownVote{this.state.downvote}</p>
+        <p>{this.state.postId}</p>
+        <Switch>
+          <Route exact path='/' component={Forum} />
+          <Route path='/create-post' component={CreatePost} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
