88ee04f1f2e817e7fb3268bfc9438821f3566584
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-19 13:50:46 -0800

    Change CreatePost to class and add Ref to it.

--- src/CreatePost.jsx
+++ src/CreatePost.jsx
@@ -1,61 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { v4 } from 'uuid'
 import PropTypes from 'prop-types'
 import { Redirect } from 'react-router-dom'
 
-const CreatePost = props => {
-    // state = {
-    //     redirect: false,
-    // }
+class CreatePost extends Component {
+    state = {
+        toHome: false,
+        text: ''
+    }
+    // Create Ref to a class component ref: https://reactjs.org/docs/refs-and-the-dom.html
+    _title = React.createRef()
+    _body = React.createRef()
     
-    let _title = null
-    let _body = null
-    let _submitted = false
 
-    const handleNewForm = event => {
+    handleNewForm = event => {
         event.preventDefault()
-        props.onCreateNewPost({
-            title: _title.value,
-            body: _body.value,
+        this.props.onCreateNewPost({
+            title: this._title.value,
+            body: this._body.value,
             id: v4(),
             upvote: 0,
             downvote: 0
         })
-        console.log(_submitted.value)
-        _submitted = true
+
+        this.setState({
+            toHome: true
+        })
     }
 
-    // setRedirect = () => {
-    //     this.setState({
-    //         redirect: true
-    //     })
-    // }
+    
+    render() { 
 
-    const redirectHome = () => {
-        if(_submitted.value) return <Redirect to='/' /> 
+        if(this.state.toHome){
+            return <Redirect to='/' />
+        } 
+        
+        return(
+            <main>
+                <h1>Create New Post</h1>
+                <form onSubmit={this.handleNewForm} className='form-container'>
+                    <input
+                        type='text'
+                        placeholder='Post title'
+                        ref={input => {this._title = input}}
+                    />
+                    <textarea
+                        type='text'
+                        placeholder='Post content'
+                        ref={textarea => {this._body = textarea}}
+                    ></textarea>
+                    <button type='submit'
+                    >Post</button>
+                </form>
+            </main>
+        )
     }
-
-    return(
-        <main>
-            <h1>Create New Post</h1>
-            <form onSubmit={handleNewForm} className='form-container'>
-                <input
-                    type='text'
-                    placeholder='Post title'
-                    ref={input => {_title = input}}
-                />
-                <textarea
-                    type='text'
-                    placeholder='Post content'
-                    ref={textarea => {_body = textarea}}
-                ></textarea>
-                {/* {this.renderRedirect()} */}
-                <button type='submit'
-                    onClick={redirectHome}
-                >Post</button>
-            </form>
-        </main>
-    )
 }
 
 CreatePost.propTypes = {
