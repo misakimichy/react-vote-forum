ef3478d38b1ceeb6eb2efc26f0d3517411deff7c
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-19 17:08:26 -0800

    Fix error on NotFound component.

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -10,23 +10,6 @@ const NotFound = props => {
                 <p>The page "{props.location.pathname}" does not exist.</p>
                 <Link to='/'>Home</Link>
             </div>
-            <style jsx>{`
-                .not-found {
-                    background: url('https://http.cat/404') no-repeat center center fixed;
-                    background-size: cover;
-                    height 100vh;
-                    overflow: hidden
-                }
-
-                .container {
-                    width: 80%;
-                    margin: 0 auto;
-                    padding: 1em;
-                    text-align: center;
-                    background-color: rgba(255, 255, 255, 0.3);
-                    border-radius: 4px;
-                }
-            `}</style>
         </main>
     )
 }
